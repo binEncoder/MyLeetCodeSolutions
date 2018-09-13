c4620aaf9062773496ffba96b8c54fa07ed926a7
wy <wy@stu.localdomain>
2018-09-13 22:47:56 +0800

    新提交Count Primers的解法

---
+++ leetcode/Solutions/Count Primers.h
@@ -0,0 +1,22 @@
+#include "common.h"
+
+class Solution_CountPrimes {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) return 0;
+		vector<bool> passed(n, false);
+		int sum = 1;
+		int upper = (int)sqrt(n);
+		for (int i = 3; i < n; i += 2) {
+			if ( !passed[i] ) {
+				sum++;
+				//avoid overflow
+				if (i > upper) continue;
+				for (int j = i*i; j < n; j += i) {
+					passed[j] = true;
+				}
+			}
+		}
+		return sum;
+    }
+};
